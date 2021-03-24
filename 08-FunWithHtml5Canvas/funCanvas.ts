75acdb37a3221abf4d06694cdcdb005ceea9bb4b
kengru <kengrullon@gmail.com>
2021-03-24 18:54:24 -0400

    Added challenge number 8 about canvas.

---
+++ 08-FunWithHtml5Canvas/funCanvas.ts
@@ -0,0 +1,47 @@
+const canvas = document.querySelector<HTMLCanvasElement>("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 100;
+ctx.globalCompositeOperation = "screen";
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(event: MouseEvent) {
+  if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [event.offsetX, event.offsetY];
+
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+}
+
+canvas.addEventListener("mousedown", (event: MouseEvent) => {
+  isDrawing = true;
+  [lastX, lastY] = [event.offsetX, event.offsetY];
+});
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
