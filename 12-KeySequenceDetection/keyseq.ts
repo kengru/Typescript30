8d674fb237f2ded94b0da1a66a89dbeaf7ddc23c
Kendry Grullón <kengrullon@gmail.com>
2021-04-13 16:15:39 -0400

    Fixing typings issue and build command.

--- 12-KeySequenceDetection/keyseq.ts
+++ 12-KeySequenceDetection/keyseq.ts
@@ -1,8 +1,13 @@
 const pressed = [];
 const secret = "laura";
 
+function cornify_add() {
+  throw new Error("Function not implemented.");
+}
+
 window.addEventListener("keyup", (event) => {
   pressed.push(event.key);
+  console.log(event.key);
   pressed.splice(-secret.length - 1, pressed.length - secret.length);
   if (pressed.join("").includes(secret)) {
     console.log("Yes!");
